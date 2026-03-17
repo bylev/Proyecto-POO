a766c398842c5e01b8eb9a5f8f0bf1922e031ff6
Michelle Camara Gonzalez <michelle.camara@tecdesoftware.edu.mx>
2026-03-17 16:08:23 -0600

    Delete src directory

--- src/interfaces/Durable.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Durable {
-    void reducirDurabilidad(int cantidad);
-    void reparar(int cantidad);
-    boolean estaRota();
-}
--- src/interfaces/Vida.java
+++
@@ -1,6 +0,0 @@
-package interfaces;
-
-public interface Vida {
-    void recibirDanio(int cantidad);
-    boolean estaVivo();
-}
