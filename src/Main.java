bac8afb696b6cda7208cbab9631337684003e475
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-03-19 17:43:37 -0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,93 @@ import items.Arma;
 import items.Armadura;
 import items.Consumible;
 import Sistema.SistemaJuego;
-import excepciones.*;
-import personajes.*;
-import enemigos.*;
+import excepciones.ManaInsuficienteException;
+import excepciones.ObjetoNoPosibleException;
+import personajes.Guerrero;
+import personajes.Mago;
+import enemigos.Dragon;
+import enemigos.Esqueleto;
 
+/**
+ * Clase principal del juego RPG por consola.
+ * 
+ * Demuestra el uso de:
+ * - Herencia y clases abstractas (Personaje, Enemigo)
+ * - Interfaces (Vida, Durable)
+ * - Polimorfismo (atacar, bloquear, equiparEn)
+ * - Manejo de excepciones personalizadas (ManaInsuficienteException,
+ * ObjetoNoPosibleException)
+ * - Composición (SistemaJuego -> GestorPersonajes, GestorEnemigos,
+ * GestorBatallas)
+ * - Inventario por personaje
+ */
 public class Main {
-    public static void main(String[] args) throws ManaInsuficienteException, ObjetoNoPosibleException {
+
+    public static void main(String[] args) {
 
         SistemaJuego juego = new SistemaJuego("Mundo RPG");
 
         Guerrero aragorn = new Guerrero("Aragorn", 5, 100, 10, 10);
         Mago gandalf = new Mago("Gandalf", 5, 100, 100);
-        Dragon dragon = new Dragon(8);
-        Esqueleto esqueleto = new Esqueleto(8);
 
         juego.registrarPersonaje(aragorn);
         juego.registrarPersonaje(gandalf);
+
+        Dragon dragon = new Dragon(8);
+        Esqueleto esqueleto = new Esqueleto(8);
+
         juego.registrarEnemigo(dragon);
         juego.registrarEnemigo(esqueleto);
 
-        Arma espada = new Arma("Espada", 1, 15, 100);
+        Arma espada = new Arma("Espada de Hierro", 1, 15, 100);
         Armadura cota = new Armadura("Cota de Malla", 1, 5, 100);
-        Consumible pocG = new Consumible("Pocion", 1, 30);
-        Consumible pocM = new Consumible("Pocion", 3, 30);
+        Consumible pocG = new Consumible("Poción de Vida", 1, 30);
+        Consumible pocM = new Consumible("Poción de Mana", 3, 30);
 
         juego.darArma(aragorn, espada);
         juego.darArmadura(aragorn, cota);
         juego.darConsumible(aragorn, pocG);
         juego.darConsumible(gandalf, pocM);
 
-        juego.equiparItem(aragorn, espada);
-        juego.equiparItem(aragorn, cota);
-        juego.equiparItem(aragorn, pocG);
-        juego.equiparItem(gandalf, pocM);
+        try {
+            juego.equiparItem(aragorn, espada);
+            juego.equiparItem(aragorn, cota);
+            juego.equiparItem(aragorn, pocG);
+            juego.equiparItem(gandalf, pocM);
+        } catch (ObjetoNoPosibleException e) {
+            System.out.println("[ERROR al equipar] " + e.getMessage());
+        }
 
-        // mostrar inventario
         juego.mostrarInventario(aragorn);
         juego.mostrarInventario(gandalf);
 
         juego.mostrarEstado();
+        try {
+            juego.iniciarBatalla(aragorn, dragon);
+        } catch (ManaInsuficienteException e) {
+            System.out.println("[ERROR en batalla] " + e.getMessage());
+        }
 
-        juego.iniciarBatalla(aragorn, dragon);
-        juego.iniciarBatalla(gandalf, esqueleto);
+        try {
+            juego.iniciarBatalla(gandalf, esqueleto);
+        } catch (ManaInsuficienteException e) {
+            System.out.println("[ERROR en batalla] " + e.getMessage());
+        }
 
-        // usar consumible
-        juego.usarConsumible(aragorn);
-        juego.usarConsumible(gandalf);
+        try {
+            juego.usarConsumible(aragorn);
+        } catch (ObjetoNoPosibleException e) {
+            System.out.println("[ERROR al usar consumible - Aragorn] " + e.getMessage());
+        }
+
+        try {
+            juego.usarConsumible(gandalf);
+        } catch (ObjetoNoPosibleException e) {
+            System.out.println("[ERROR al usar consumible - Gandalf] " + e.getMessage());
+        }
 
         juego.mostrarEstado();
-        juego.getTotalBatallas();
+
+        System.out.println("\n>>> Total de batallas registradas: " + juego.getTotalBatallas());
     }
 }
