3b7042dc5e35dcd5528acecae869146f12e94029
datacamaleon-netizen <datacamaleon@gmail.com>
2026-03-19 11:32:23 -0600

    Update Armadura.java
    Actualizado por Gustavo

--- src/items/Armadura.java
+++ src/items/Armadura.java
@@ -1,5 +1,6 @@
 package items;
 
+import ExcepcionesPersonalizadas.ObjetoNoPosibleException;
 import interfaces.Durable;
 import personajes.Personaje;
 
@@ -10,63 +11,31 @@ public class Armadura extends Item implements Durable {
 
     public Armadura(String nombre, int cantidad, int defensa, int durabilidadMaxima) {
         super(nombre, cantidad);
-        if (defensa <= 0)
-            throw new IllegalArgumentException("La defensa debe ser mayor a 0.");
-        if (durabilidadMaxima <= 0)
-            throw new IllegalArgumentException("La durabilidad máxima debe ser mayor a 0.");
-
+        if (defensa <= 0) throw new IllegalArgumentException("La defensa debe ser mayor a 0.");
         this.defensa = defensa;
-        this.durabilidadActual = durabilidadMaxima;
         this.durabilidadMaxima = durabilidadMaxima;
+        this.durabilidadActual = durabilidadMaxima;
     }
 
-    /* Getters */
-    public int getDefensa() {
-        return defensa;
+    @Override
+    public void equiparEn(Personaje p) throws ObjetoNoPosibleException {
+        if (estaRota()) {
+            throw new ObjetoNoPosibleException(this.durabilidadActual);
+        } else {
+            p.setArmadura(this);
+        }
     }
 
-    public int getDurabilidadActual() {
-        return durabilidadActual;
-    }
-
-    public int getDurabilidadMaxima() {
-        return durabilidadMaxima;
+    @Override
+    public void usar() throws ObjetoNoPosibleException {
+        System.out.println("Revisando estado de la armadura: " + nombre);
     }
 
     @Override
     public void reducirDurabilidad(int cantidad) {
-        durabilidadActual -= cantidad;
-        if (durabilidadActual < 0)
-            durabilidadActual = 0;
+        durabilidadActual = Math.max(0, durabilidadActual - cantidad);
     }
 
     @Override
-    public boolean estaRota() {
-        return durabilidadActual <= 0;
-    }
-
-    @Override
-    public void usar() {
-        if (getCantidad() <= 0) {
-            System.out.println("No cuentas con este item en tu inventario.");
-            return;
-        }
-
-        if (estaRota()) {
-            System.out.println("El armadura " + getNombre() + " está rota y no puede usarse.");
-        } else
-            System.out.println("Armadura: " + getNombre() + " | Estado: "
-                    + (estaRota() ? "ROTA" : durabilidadActual + "/" + durabilidadMaxima));
-    }
-
-    @Override
-    public void equiparEn(Personaje p) {
-        if (estaRota()) {
-            System.out.println("No puedes equipar " + getNombre() + " porque está rota.");
-        } else {
-            p.setArmadura(this);
-            System.out.println(p.getNombre() + " equipó la armadura " + getNombre());
-        }
-    }
-
+    public boolean estaRota() { return durabilidadActual <= 0; }
 }
