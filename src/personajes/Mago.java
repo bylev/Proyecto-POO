05e0444642ced4414e3c9a75d382b5a806ae418f
datacamaleon-netizen <datacamaleon@gmail.com>
2026-03-19 11:24:54 -0600

    Update Mago.java
    Actualizado por Gustavo

--- src/personajes/Mago.java
+++ src/personajes/Mago.java
@@ -1,6 +1,8 @@
 package personajes;
 
+import ExcepcionesPersonalizadas.ManaInsuficienteException;
 import enemigos.Enemigo;
+import items.Arma;
 
 public class Mago extends Personaje {
     private int mana;
@@ -8,49 +10,33 @@ public class Mago extends Personaje {
 
     public Mago(String nombre, int nivel, int vidaMaxima, int total_mana) {
         super(nombre, nivel, vidaMaxima);
-        if (total_mana <= 0)
-            throw new IllegalArgumentException("El mana debe ser mayor a 0.");
+        if (total_mana <= 0) throw new IllegalArgumentException("El mana debe ser mayor a 0.");
         this.total_mana = total_mana;
         this.mana = total_mana;
     }
 
-    /* Getters */
-    public int getMana() {
-        return mana;
-    }
-
-    public int getTotal_mana() {
-        return total_mana;
-    }
-
-    /* Métodos */
-
     @Override
-    public void atacar(Enemigo e) {
+    public void atacar(Enemigo e) throws ManaInsuficienteException {
+        if (this.mana < 10) throw new ManaInsuficienteException(mana);
         int danioMagico = 20 + (getNivel() * 2);
         mana -= 10;
-        System.out.println(
-                getNombre() + " ataca a " + e.getNombre() + " con su magia infligiendo " + danioMagico
-                        + " puntos de daño.");
+        System.out.println(nombre + " lanza un hechizo a " + e.getNombre() + " infligiendo " + danioMagico);
         e.recibirDanio(danioMagico);
     }
 
     @Override
     public void bloquear() {
         setBloqueando(true);
-        System.out.println(getNombre() + " bloquea el ataque.");
+        System.out.println(nombre + " crea un escudo mágico.");
     }
 
     @Override
     public String toString() {
+        Arma a = getArma();
         return "=============== Mago ===============\n" +
-                "Nombre: " + getNombre() + "\n" +
-                "Nivel: " + getNivel() + "\n" +
-                "Vida: " + getVidaActual() + "/" + getVidaMaxima() + "\n" +
-                "Daño: " + getDanio() + "\n" +
-                "Defensa: " + getDefensa() + "\n" +
-                "Armadura: " + (getArmadura() != null ? getArmadura().getNombre() : "Ninguna") + "\n" +
-                "Consumible: " + (getConsumible() != null ? getConsumible().getNombre() : "Ninguno") + "\n" +
-                "=========================================";
+                "Nombre: " + nombre + "\n" +
+                "Mana: " + mana + "/" + total_mana + "\n" +
+                "Arma: " + (a != null ? a.getNombre() : "Ninguna") + "\n" +
+                "====================================";
     }
 }
